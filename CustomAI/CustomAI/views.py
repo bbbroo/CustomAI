7f7fe64caa79f58cba67888d3dc8a3a95720f853
brookinsbarry <72997929+brookinsbarry@users.noreply.github.com>
2023-01-16 17:38:42 -0600

    Replaced "views" content

--- CustomAI/CustomAI/views.py
+++ CustomAI/CustomAI/views.py
@@ -1,37 +1,47 @@
-"""
-Routes and views for the flask application.
-"""
+import os
+import openai
+import config
 
-from datetime import datetime
-from flask import render_template
-from CustomAI import app
 
-@app.route('/')
-@app.route('/home')
-def home():
-    """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
+openai.api_key = config.OPENAI_API_KEY
+
+
+def generateBlogTopics(prompt1):
+    response = openai.Completion.create(
+      engine="davinci-instruct-beta-v3",
+      prompt="Generate blog topics on: {}. \n \n 1.  ".format(prompt1),
+      temperature=0.7,
+      max_tokens=100,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0
     )
 
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
+    return response['choices'][0]['text']
+
+def generateBlogSections(prompt1):
+    response = openai.Completion.create(
+      engine="davinci-instruct-beta-v3",
+      prompt="Expand the blog title in to high level blog sections: {} \n\n- Introduction: ".format(prompt1),
+      temperature=0.6,
+      max_tokens=100,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0
     )
 
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
+    return response['choices'][0]['text']
+
+
+def blogSectionExpander(prompt1):
+    response = openai.Completion.create(
+      engine="davinci-instruct-beta-v3",
+      prompt="Expand the blog section in to a detailed professional , witty and clever explanation.\n\n {}".format(prompt1),
+      temperature=0.7,
+      max_tokens=200,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0
     )
+
+    return response['choices'][0]['text']
