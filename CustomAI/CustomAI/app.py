e29824151613fac72a0ef8c121e7347d9c9acd5e
brookinsbarry <72997929+brookinsbarry@users.noreply.github.com>
2023-01-16 18:49:21 -0600

    Updated packages and names

--- CustomAI/CustomAI/app.py
+++ CustomAI/CustomAI/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import config
-import blog
+import views
 
 
 def page_not_found(e):
@@ -19,17 +19,17 @@ def index():
     if request.method == 'POST':
         if 'form1' in request.form:
             prompt = request.form['blogTopic']
-            blogT = blog.generateBlogTopics(prompt)
+            blogT = views.generateBlogTopics(prompt)
             blogTopicIdeas = blogT.replace('\n', '<br>')
 
         if 'form2' in request.form:
             prompt = request.form['blogSection']
-            blogT = blog.generateBlogSections(prompt)
+            blogT = views.generateBlogSections(prompt)
             blogSectionIdeas = blogT.replace('\n', '<br>')
 
         if 'form3' in request.form:
             prompt = request.form['blogExpander']
-            blogT = blog.blogSectionExpander(prompt)
+            blogT = views.blogSectionExpander(prompt)
             blogExpanded = blogT.replace('\n', '<br>')
 
         if 'form4' in request.form:
@@ -42,4 +42,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='8888', debug=True)
+    app.run(host='127.0.0.1', port='8888', debug=True)
